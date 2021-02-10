8378fea530af275654644643185529c0a789c477
Eder Sant'ana <edersana@gmail.com>
2021-02-10 13:02:15 -0300

    mostrando Alert

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,6 +1,8 @@
 package gui;
 
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 
 /*
@@ -13,7 +15,7 @@ public class ViewController {
 	
 	@FXML
 	public void onBtnTestAction() {
-		System.out.println("Click");
+		Alerts.showAlert("Alert title", null, "hello", AlertType.WARNING);
 	}
 	
 
